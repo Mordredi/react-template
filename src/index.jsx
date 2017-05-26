a8b0abac99ca454477d78818e496447cded09a25
Arthur <arthur.p.wright@gmail.com>
2017-05-25 21:33:01 -0400

    Set up hot loading

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,27 @@
+/* eslint import/no-extraneous-dependencies: 'off' */
+
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
 import App from './app/App';
 
 const root = document.getElementById('root');
 
-render(<App />, root);
+const bootstrap = (Component) => {
+  render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root,
+  );
+};
+
+bootstrap(App);
+
+if (module.hot) {
+  module.hot.accept('./app/App', () => {
+    bootstrap(App);
+  });
+}
+
