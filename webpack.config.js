68cd49e97c48091abf7cb768bc9c86f40b49b7e0
Arthur <arthur.p.wright@gmail.com>
2017-05-28 11:36:45 -0400

    Added sass css webpack support

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,12 @@ module.exports = () => ({
       }, {
         test: /.html$/,
         use: ['html-loader'],
+      }, {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      }, {
+        test: /.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
